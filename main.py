1e4ec24935213063749741cf37ae32f467a2b82b
William Budington <bill@eff.org>
2015-11-02 15:12:09 -0800

    Adding about page

--- main.py
+++ main.py
@@ -9,5 +9,9 @@ def index():
 def privacy():
     return render_template('privacy.html', title="Privacy Policy")
 
+@app.route("/about")
+def about():
+    return render_template('about.html', title="About")
+
 if __name__ == "__main__":
     app.run()
