8bc38acdb61e7f1e53d5e4019214e7381330ef22
William Budington <bill@eff.org>
2015-11-02 15:23:18 -0800

    Adding self-defense page

--- main.py
+++ main.py
@@ -17,5 +17,9 @@ def about():
 def faq():
     return render_template('faq.html', title="Frequently asked questions about Panopticlick")
 
+@app.route("/self-defense")
+def self_defense():
+    return render_template('self-defense.html', title="Self-Defense")
+
 if __name__ == "__main__":
     app.run()
