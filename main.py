36f690bb54c737bf27696904d1ae84b367fba019
William Budington <bill@eff.org>
2015-11-02 15:34:29 -0800

    Adding robots.txt

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -20,6 +20,10 @@ def faq():
 @app.route("/self-defense")
 def self_defense():
     return render_template('self-defense.html', title="Self-Defense")
+
+@app.route('/robots.txt')
+def static_from_root():
+    return send_from_directory(app.static_folder, request.path[1:])
 
 if __name__ == "__main__":
     app.run()
