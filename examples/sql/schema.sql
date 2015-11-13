8bf4338ea59788db4b04e9190cb06f994bca83a8
William Budington <bill@eff.org>
2015-11-12 17:23:05 -0800

    Add epoch_total to totals

--- examples/sql/schema.sql
+++ examples/sql/schema.sql
@@ -58,6 +58,7 @@ CREATE TABLE `totals` (
   `variable` varchar(32) NOT NULL DEFAULT '',
   `value` varchar(255) NOT NULL DEFAULT '',
   `total` int(11) NOT NULL DEFAULT '0',
+  `epoch_total` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `variable` (`variable`,`value`(32))
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
