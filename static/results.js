b5a0b3c44f6e79749cdd1478e8351c7678a929f8
Artemis Schatzkin <schatzkin@eff.org>
2020-08-31 16:11:42 -0700

    fixed the "this" for results detailed view

--- static/results.js
+++ static/results.js
@@ -1,21 +1,34 @@
 $(document).ready(function(){
   setTimeout(function(){
   $('.results-table h4').each(function(i){
+    //function getText() {
+    //  $.ajax({
+    //    url : "/static/results-text/user-agent.txt",
+    //    dataType: "text",
+    //    success : function (data) {
+    //      $(".text").html(data);
+    //    }
+    //  });
+    //}
       if ($(this).html().includes('User Agent')) {
+        var self = $(this).html();
         $.ajax({
           url : "/static/results-text/user-agent.txt",
+          context: this,
           dataType: "text",
           success : function (data) {
-            $(".text").html(data);
+            $(this).parent().find(".text.detailed").html(data);
           }
-        });
+        })
       }
       if ($(this).html().includes('HTTP_ACCEPT Headers')) {
+        var self = $(this).html();
         $.ajax({
           url : "/static/results-text/http-accept-headers.txt",
+          context: this,
           dataType: "text",
           success : function (data) {
-            $(".text").html(data);
+              $(this).parent().find(".text.detailed").html(data);
           }
         });
       }
