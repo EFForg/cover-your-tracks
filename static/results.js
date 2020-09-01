960bdce48358d4f40af3090a89b91d856ede2df9
Artemis Schatzkin <schatzkin@eff.org>
2020-09-01 16:28:27 -0700

    refactoring result text with getText function

--- static/results.js
+++ static/results.js
@@ -1,36 +1,25 @@
 $(document).ready(function(){
   setTimeout(function(){
   $('.results-table h4').each(function(i){
-    //function getText() {
-    //  $.ajax({
-    //    url : "/static/results-text/user-agent.txt",
-    //    dataType: "text",
-    //    success : function (data) {
-    //      $(".text").html(data);
-    //    }
-    //  });
-    //}
+    function getText(file,self) {
+      $.ajax({
+        url : file,
+        context: self,
+        dataType: "text",
+        success : function (data) {
+          self.parent().find(".text").html(data);
+        }
+      })
+    }
       if ($(this).html().includes('User Agent')) {
-        var self = $(this).html();
-        $.ajax({
-          url : "/static/results-text/user-agent.txt",
-          context: this,
-          dataType: "text",
-          success : function (data) {
-            $(this).parent().find(".text.detailed").html(data);
-          }
-        })
+        var filename = '/static/results-text/user-agent.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
       }
       if ($(this).html().includes('HTTP_ACCEPT Headers')) {
-        var self = $(this).html();
-        $.ajax({
-          url : "/static/results-text/http-accept-headers.txt",
-          context: this,
-          dataType: "text",
-          success : function (data) {
-              $(this).parent().find(".text.detailed").html(data);
-          }
-        });
+        var filename = '/static/results-text/http-accept-headers.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
       }
     });
     $('.detailed').hide();
