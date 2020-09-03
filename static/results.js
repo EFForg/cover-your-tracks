8f72307bce80c545c632ba45f1b6fe3bf4977f4a
Artemis Schatzkin <schatzkin@eff.org>
2020-09-03 15:47:40 -0700

    added a bunch of results text files, updated results.js

--- static/results.js
+++ static/results.js
@@ -21,6 +21,91 @@ $(document).ready(function(){
         var selfname = $(this);
         getText(filename, selfname);
       }
+      if ($(this).html().includes('Browser Plugin')) {
+        var filename = '/static/results-text/browser-plugin-details.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('AudioContext')) {
+        var filename = '/static/results-text/audiocontext.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('Cookies Enabled')) {
+        var filename = '/static/results-text/cookies-enabled.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('Device Memory')) {
+        var filename = '/static/results-text/device-memory.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('DNT Header')) {
+        var filename = '/static/results-text/dnt-header.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('Hardware Concurrency')) {
+        var filename = '/static/results-text/hardware-concurrency.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('Hash of canvas fingerprint')) {
+        var filename = '/static/results-text/hash-of-canvas.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('Hash of WebGL fingerprint')) {
+        var filename = '/static/results-text/hash-of-webgl.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('Limited supercookie ')) {
+        var filename = '/static/results-text/limited-super-cookie.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('Platform')) {
+        var filename = '/static/results-text/platform.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('Screen Size')) {
+        var filename = '/static/results-text/screen-size.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('System Fonts')) {
+        var filename = '/static/results-text/system-fonts.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('Time Zone')) {
+        var filename = '/static/results-text/time-zone.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('Time Zone Offset')) {
+        var filename = '/static/results-text/time-zone-offset.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('Touch Support')) {
+        var filename = '/static/results-text/touch-support.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('User Agent')) {
+        var filename = '/static/results-text/user-agent.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
+      if ($(this).html().includes('WebGL Vendor')) {
+        var filename = '/static/results-text/webgl-vendor.txt';
+        var selfname = $(this);
+        getText(filename, selfname);
+      }
     });
     $('.detailed').hide();
     $('#default-button').on( 'click', function(e) {
