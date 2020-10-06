d5d4b3c0b5c4e73d72585078321fd64b9c9bdfc1
Artemis Schatzkin <schatzkin@eff.org>
2020-10-06 16:34:40 -0700

    learning sliders now showing correct values

--- static/learn.js
+++ static/learn.js
@@ -1,11 +1,38 @@
 $(document).ready(function(){
   var trackerInput = document.getElementById('tracker-input');
   var trackerOutput  = document.getElementById('tracker-output');
+  var jsInput = document.getElementById('js-input');
+  var jsOutput  = document.getElementById('js-output');
+  var defaultsInput = document.getElementById('defaults-input');
+  var defaultsOutput  = document.getElementById('defaults-output');
+  var resistantInput = document.getElementById('resistant-input');
+  var resistantOutput  = document.getElementById('resistant-output');
   trackerInput.onchange = function(){
      if (trackerInput.value == '1') {
        trackerOutput.value = "installed"
      } else {
        trackerOutput.value = "not installed"
+     }
+  }
+  jsInput.onchange = function(){
+     if (jsInput.value == '1') {
+       jsOutput.value = "enabled"
+     } else {
+       jsOutput.value = "not enabled"
+     }
+  }
+  defaultsInput.onchange = function(){
+     if (defaultsInput.value == '1') {
+       defaultsOutput.value = "yes"
+     } else {
+       defaultsOutput.value = "no"
+     }
+  }
+  resistantInput.onchange = function(){
+     if (resistantInput.value == '1') {
+       resistantOutput.value = "yes"
+     } else {
+       resistantOutput.value = "no"
      }
   }
 
