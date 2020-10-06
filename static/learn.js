15a32c0c91552c830112f26ee04b0b4bdae592f5
Artemis Schatzkin <schatzkin@eff.org>
2020-10-06 15:08:26 -0700

    tracker blocker slider value working

--- static/learn.js
+++ static/learn.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
-  document.registrationForm.trackerInput.oninput = function(){
-     document.registrationForm.trackerOurpur.value = document.registrationForm.trackerInput.value;
+  var trackerInput = document.getElementById('tracker-input');
+  var trackerOutput  = document.getElementById('tracker-output');
+  trackerInput.onchange = function(){
+     if (trackerInput.value == '1') {
+       trackerOutput.value = "installed"
+     } else {
+       trackerOutput.value = "not installed"
+     }
   }
 
 });
