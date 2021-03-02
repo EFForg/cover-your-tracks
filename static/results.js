c63c165668df3073a6641ebd2220b9a085339829
Artemis Schatzkin <schatzkin@eff.org>
2021-03-02 10:02:25 -0800

    reordering results per specs

--- static/results.js
+++ static/results.js
@@ -21,87 +21,87 @@ $(document).ready(function(){
       }
       if ($(this).html().includes('User Agent')) {
         var filename = '/static/results-text/user-agent.txt';
-        var selfname = $(this).prepend('<span>header <span>');
+        var selfname = $(this).prepend('<span>1header <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('HTTP_ACCEPT Headers')) {
         var filename = '/static/results-text/http-accept-headers.txt';
-                var selfname = $(this).prepend('<span>header <span>');
+                var selfname = $(this).prepend('<span>1header <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('Browser Plugin')) {
         var filename = '/static/results-text/browser-plugin-details.txt';
-        var selfname = $(this).prepend('<span>browser <span>');
+        var selfname = $(this).prepend('<span>2browser <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('AudioContext')) {
         var filename = '/static/results-text/audiocontext.txt';
-        var selfname = $(this).prepend('<span>fingerprint <span>');
+        var selfname = $(this).prepend('<span>3fingerprint <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('Cookies Enabled')) {
         var filename = '/static/results-text/cookies-enabled.txt';
-        var selfname = $(this).prepend('<span>browser <span>');
+        var selfname = $(this).prepend('<span>2browser <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('Device Memory')) {
         var filename = '/static/results-text/device-memory.txt';
-        var selfname = $(this).prepend('<span>hardware <span>');
+        var selfname = $(this).prepend('<span>4hardware <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('DNT Header')) {
         var filename = '/static/results-text/dnt-header.txt';
-                var selfname = $(this).prepend('<span>header <span>');
+                var selfname = $(this).prepend('<span>1header <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('Hardware Concurrency')) {
         var filename = '/static/results-text/hardware-concurrency.txt';
-        var selfname = $(this).prepend('<span>hardware <span>');
+        var selfname = $(this).prepend('<span>4hardware <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('Hash of canvas fingerprint')) {
         var filename = '/static/results-text/hash-of-canvas.txt';
-        var selfname = $(this).prepend('<span>fingerprint <span>');
+        var selfname = $(this).prepend('<span>3fingerprint <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('Hash of WebGL fingerprint')) {
         var filename = '/static/results-text/hash-of-webgl.txt';
-        var selfname = $(this).prepend('<span>fingerprint <span>');
+        var selfname = $(this).prepend('<span>3fingerprint <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('Limited supercookie ')) {
         var filename = '/static/results-text/limited-super-cookie.txt';
-        var selfname = $(this).prepend('<span>browser <span>');
+        var selfname = $(this).prepend('<span>2browser <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('Platform')) {
         var filename = '/static/results-text/platform.txt';
-        var selfname = $(this).prepend('<span>hardware <span>');
+        var selfname = $(this).prepend('<span>4hardware <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('Screen Size')) {
         var filename = '/static/results-text/screen-size.txt';
-        var selfname = $(this).prepend('<span>browser <span>');
+        var selfname = $(this).prepend('<span>2browser <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('System Fonts')) {
         var filename = '/static/results-text/system-fonts.txt';
-        var selfname = $(this).prepend('<span>fingerprint <span>');
+        var selfname = $(this).prepend('<span>3fingerprint <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('Time Zone')) {
         var filename = '/static/results-text/time-zone.txt';
-        var selfname = $(this).prepend('<span>browser <span>');
+        var selfname = $(this).prepend('<span>2browser <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('Time Zone Offset')) {
         var filename = '/static/results-text/time-zone-offset.txt';
-        var selfname = $(this).prepend('<span>browser <span>');
+        var selfname = $(this).prepend('<span>2browser <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('Touch Support')) {
         var filename = '/static/results-text/touch-support.txt';
-        var selfname = $(this).prepend('<span>hardware <span>');
+        var selfname = $(this).prepend('<span>4hardware <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('User Agent')) {
@@ -111,22 +111,22 @@ $(document).ready(function(){
       }
       if ($(this).html().includes('WebGL Vendor')) {
         var filename = '/static/results-text/webgl-vendor.txt';
-        var selfname = $(this).prepend('<span>fingerprint <span>');
+        var selfname = $(this).prepend('<span>3fingerprint <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('Language')) {
         var filename = '/static/results-text/language.txt';
-        var selfname = $(this).prepend('<span>browser <span>');
+        var selfname = $(this).prepend('<span>2browser <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('Ad Blocker Used')) {
         var filename = '/static/results-text/ad-blocker.txt';
-        var selfname = $(this).prepend('<span>browser <span>');
+        var selfname = $(this).prepend('<span>2browser <span>');
         getText(filename, selfname);
       }
       if ($(this).html().includes('CPU Class')) {
         var filename = '/static/results-text/cpu-class.txt';
-        var selfname = $(this).prepend('<span>hardware <span>');
+        var selfname = $(this).prepend('<span>4hardware <span>');
         getText(filename, selfname);
       }
     });
